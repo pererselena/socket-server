8800d773694c25bb1114c5d8e2a00108f5ea87bf
Elena Perers <elena.perers@chasacademy.se>
2019-10-06 21:23:35 +0200

    missed require...

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
