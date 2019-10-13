5d5ed7f3f6125032cb255a9a91eea022f8a1e954
Elena Perers <elena.perers@chasacademy.se>
2019-10-13 13:36:20 +0200

    Added dotenv and code to connect to db

---
+++ db.js
@@ -0,0 +1,73 @@
+/**
+ * Connect to the database and search using a criteria.
+ */
+"use strict";
+
+dotenv.config()
+
+// MongoDB
+const mongo = require("mongodb").MongoClient;
+const dsn = process.env.CHATTDB_DSN || "mongodb://localhost:27017/chatt";
+
+// Express server
+const port = process.env.CHATTDB_PORT || 1337;
+const express = require("express");
+const app = express();
+
+
+
+// Just for testing the sever
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+
+
+// Return a JSON object with list of all documents within the collection.
+app.get("/chatt", async (request, response) => {
+    try {
+        let res = await findInCollection(dsn, "history", {}, {}, 0);
+
+        console.log(res);
+        response.json(res);
+    } catch (err) {
+        console.log(err);
+        response.json(err);
+    }
+});
+
+
+
+// Startup server and liten on port
+app.listen(port, () => {
+    console.log(`Server is listening on ${port}`);
+    console.log(`DSN is: ${dsn}`);
+});
+
+
+
+/**
+ * Find documents in an collection by matching search criteria.
+ *
+ * @async
+ *
+ * @param {string} dsn        DSN to connect to database.
+ * @param {string} colName    Name of collection.
+ * @param {object} criteria   Search criteria.
+ * @param {object} projection What to project in results.
+ * @param {number} limit      Limit the number of documents to retrieve.
+ *
+ * @throws Error when database operation fails.
+ *
+ * @return {Promise<array>} The resultset as an array.
+ */
+async function findInCollection(dsn, colName, criteria, projection, limit) {
+    const client = await mongo.connect(dsn);
+    const db = await client.db();
+    const col = await db.collection(colName);
+    const res = await col.find(criteria, projection).limit(limit).toArray();
+
+    await client.close();
+
+    return res;
+}
